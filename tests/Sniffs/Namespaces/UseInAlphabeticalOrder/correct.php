cea6dec89cb9b827cc5d1cab984d50f13e3cc72f
Tomas Votruba <tomas.vot@gmail.com>
2015-12-30 12:06:27 +0100

    fix UseInAlphabeticalOrder sniff

--- tests/Sniffs/Namespaces/UseInAlphabeticalOrder/correct.php
+++ tests/Sniffs/Namespaces/UseInAlphabeticalOrder/correct.php
@@ -4,11 +4,10 @@ use A;
 use B;
 use D;
 use PHPUnit_Framework_TestCase;
-use X;
 use Symotion\DoctrineQueryStats\Analytics\DataCollector;
 use Symotion\DoctrineQueryStats\Tracy\Panel;
 use SymotionTests\ContainerFactory;
-
+use X;
 
 class Presenter
 {
