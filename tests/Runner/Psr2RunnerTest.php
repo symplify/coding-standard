8dfa9644d43f18965ac491f2b360f8a54956fcc7
Tomas Votruba <tomas.vot@gmail.com>
2016-01-01 19:54:54 +0100

    tests: add for runners

---
+++ tests/Runner/Psr2RunnerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Symplify\CodingStandard\Tests\Runner;
+
+use PHPUnit_Framework_TestCase;
+use Symplify\CodingStandard\Runner\Psr2Runner;
+
+final class Psr2RunnerTest extends PHPUnit_Framework_TestCase
+{
+    public function testRunForDirectory()
+    {
+        $runner = new Psr2Runner();
+        $output = $runner->runForDirectory(__DIR__.'/Psr2RunnerSource');
+
+        $this->assertStringMatchesFormat(
+            file_get_contents(__DIR__.'/Psr2RunnerSource/expected.txt'),
+            $output
+        );
+    }
+}
---
+++ tests/Runner/SymplifyRunnerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Symplify\CodingStandard\Tests\Runner;
+
+use PHPUnit_Framework_TestCase;
+use Symplify\CodingStandard\Runner\SymplifyRunner;
+
+final class SymplifyRunnerTest extends PHPUnit_Framework_TestCase
+{
+    public function testRunForDirectory()
+    {
+        $runner = new SymplifyRunner();
+        $output = $runner->runForDirectory(__DIR__.'/SymplifyRunnerSource');
+
+        $this->assertStringMatchesFormat(
+            file_get_contents(__DIR__.'/SymplifyRunnerSource/expected.txt'),
+            $output
+        );
+    }
+}
