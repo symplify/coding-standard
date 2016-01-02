e7d0c5b9f8759eab86670d5a3321b5f39e586157
Tomas Votruba <tomas.vot@gmail.com>
2016-01-02 14:59:01 +0100

    RunnerCollection added

---
+++ src/Runner/RunnerCollection.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of coding-standard
+ *
+ * Copyright (c) 2015
+ *
+ * For the full copyright and license information, please view
+ * the file LICENSE that was distributed with this source code.
+ */
+
+
+namespace Symplify\CodingStandard\Runner;
+
+
+class RunnerCollection
+{
+
+}
