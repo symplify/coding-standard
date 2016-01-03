3104fd4635867c3cdd7401061877f96122386eb3
Tomas Votruba <tomas.vot@gmail.com>
2016-01-03 15:08:58 +0100

    fix tests

--- tests/Runner/SymplifyRunnerTest.php
+++ tests/Runner/SymplifyRunnerTest.php
@@ -38,6 +38,13 @@ final class SymplifyRunnerTest extends PHPUnit_Framework_TestCase
 
     public function testFixDirectory()
     {
+        $filePath = __DIR__.'/SymplifyRunnerSource/SomeClass.php.inc';
+        $fileBackup = file_get_contents($filePath);
+
         $this->runner->fixDirectory(__DIR__.'/SymplifyRunnerSource');
+        $fixedFile = file_get_contents($filePath);
+        $this->assertNotSame($fixedFile, $fileBackup);
+
+        file_put_contents($filePath, $fileBackup);
     }
 }
