bc02b4d9141fe3d049c5924eb9eb3c48dfe42253
Tomas Votruba <tomas.vot@gmail.com>
2016-01-05 02:52:23 +0100

    MethodCommentSniff - add PHP 7 scalar typehint

--- tests/Sniffs/Commenting/MethodComment/MethodCommentSniffTest.php
+++ tests/Sniffs/Commenting/MethodComment/MethodCommentSniffTest.php
@@ -18,5 +18,6 @@ final class MethodCommentSniffTest extends PHPUnit_Framework_TestCase
         $this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__.'/correct.php.inc'));
         $this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__.'/correct2.php.inc'));
         $this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__.'/correct3.php.inc'));
+        $this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__.'/correct4.php.inc'));
     }
 }
