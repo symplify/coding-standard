66d191e2d487a8e962098bcc8135b5e140e74a32
TomasVotruba <tomas.vot@gmail.com>
2020-01-24 10:05:05 +0100

    symplify renames to lowercase

---
+++ tests/Fixer/Naming/CatchExceptionNameMatchingTypeFixer/Source/DangerousException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Fixer\Naming\CatchExceptionNameMatchingTypeFixer\Source;
+
+use Exception;
+
+final class DangerousException extends Exception
+{
+}
