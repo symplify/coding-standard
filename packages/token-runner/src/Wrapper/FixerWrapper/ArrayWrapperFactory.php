32218f6ef11c423836e03e91d918accacb9a70b8
TomasVotruba <tomas.vot@gmail.com>
2020-01-24 15:55:14 +0100

    use ignore_errors: not_found

---
+++ packages/token-runner/src/Wrapper/FixerWrapper/ArrayWrapperFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\TokenRunner\Wrapper\FixerWrapper;
+
+use PhpCsFixer\Tokenizer\Tokens;
+use Symplify\CodingStandard\TokenRunner\Analyzer\FixerAnalyzer\TokenSkipper;
+use Symplify\CodingStandard\TokenRunner\ValueObject\BlockInfo;
+
+final class ArrayWrapperFactory
+{
+    /**
+     * @var TokenSkipper
+     */
+    private $tokenSkipper;
+
+    public function __construct(TokenSkipper $tokenSkipper)
+    {
+        $this->tokenSkipper = $tokenSkipper;
+    }
+
+    public function createFromTokensAndBlockInfo(Tokens $tokens, BlockInfo $blockInfo): ArrayWrapper
+    {
+        return new ArrayWrapper($tokens, $blockInfo->getStart(), $blockInfo->getEnd(), $this->tokenSkipper);
+    }
+}
