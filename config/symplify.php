b03913635e675b38c092f6418cc350f025b0a638
TomasVotruba <tomas.vot@gmail.com>
2020-07-18 12:23:38 +0200

    open 8.2-dev

---
+++ config/symplify.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ClassNotation\FinalInternalClassFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer;
+use SlevomatCodingStandard\Sniffs\Namespaces\ReferenceUsedNamesOnlySniff;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/config.yaml');
+
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->set(FinalInternalClassFixer::class);
+
+    $services->set(ReferenceUsedNamesOnlySniff::class)
+        ->property('searchAnnotations', true)
+        ->property('allowFallbackGlobalFunctions', true)
+        ->property('allowFallbackGlobalConstants', true)
+        ->property('allowPartialUses', false);
+
+    $services->load('Symplify\CodingStandard\Fixer\\', __DIR__ . '/../src/Fixer');
+
+    $services->load('Symplify\CodingStandard\Sniffs\\', __DIR__ . '/../src/Sniffs');
+
+    $services->set(PhpdocLineSpanFixer::class);
+};
