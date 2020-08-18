a4ff6ed0196f1a58603093322494ac8acba2ee5d
TomasVotruba <tomas.vot@gmail.com>
2020-08-18 17:44:33 +0200

    no space before/after equals sign

---
+++ README.md
@@ -0,0 +1,42 @@
+# Coding Standard
+
+[![Downloads](https://img.shields.io/packagist/dt/symplify/coding-standard.svg?style=flat-square)](https://packagist.org/packages/symplify/coding-standard/stats)
+
+Set of rules for PHP_CodeSniffer, PHP-CS-Fixer and PHPStan used by Symplify projects.
+
+**They run best with [EasyCodingStandard](https://github.com/symplify/easy-coding-standard)** and **PHPStan**.
+
+## Install
+
+```bash
+composer require symplify/coding-standard --dev
+composer require symplify/easy-coding-standard --dev
+```
+
+1. Run with [ECS](https://github.com/symplify/easy-coding-standard):
+
+```bash
+vendor/bin/ecs process src --set symplify
+```
+
+2. Register rules for PHPStan:
+
+```yaml
+# phpstan.neon
+includes:
+    - vendor/symplify/coding-standard/config/symplify-rules.neon
+```
+
+## Rules Overview
+
+- [PHP_CodeSniffer sniffs](/docs/php_code_sniffer_sniffs.md)
+- [PHP CS Fixer fixers](/docs/phpcs_fixer_fixers.md)
+- [PHPStan rules](/docs/phpstan_rules.md)
+- [Object Calisthenics rules](/docs/phpstan_rules.md#object-calisthenics-rules)
+- [Cognitive Complexity rules](/docs/phpstan_rules.md#cognitive-complexity)
+
+<br>
+
+## Contributing
+
+Open an [issue](https://github.com/symplify/symplify/issues) or send a [pull-request](https://github.com/symplify/symplify/pulls) to main repository.
---
+++ src/Fixer/Annotation/DoctrineAnnotationNewlineInNestedAnnotationFixer.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Fixer\Annotation;
+
+use Doctrine\Common\Annotations\DocLexer;
+use Nette\Utils\Strings;
+use PhpCsFixer\AbstractDoctrineAnnotationFixer;
+use PhpCsFixer\Doctrine\Annotation\Token;
+use PhpCsFixer\Doctrine\Annotation\Tokens;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use Symplify\CodingStandard\TokenRunner\Analyzer\FixerAnalyzer\DoctrineBlockFinder;
+use Symplify\CodingStandard\TokenRunner\ValueObject\BlockInfo;
+
+/**
+ * @see \Symplify\CodingStandard\Tests\Fixer\Annotation\DoctrineAnnotationNewlineInNestedAnnotationFixer\DoctrineAnnotationNewlineInNestedAnnotationFixerTest
+ */
+final class DoctrineAnnotationNewlineInNestedAnnotationFixer extends AbstractDoctrineAnnotationFixer
+{
+    /**
+     * @var DoctrineBlockFinder
+     */
+    private $doctrineBlockFinder;
+
+    /**
+     * @var BlockInfo|null
+     */
+    private $currentBlockInfo;
+
+    public function __construct(DoctrineBlockFinder $doctrineBlockFinder)
+    {
+        $this->doctrineBlockFinder = $doctrineBlockFinder;
+
+        parent::__construct();
+    }
+
+    public function getPriority(): int
+    {
+        // must run before \PhpCsFixer\Fixer\DoctrineAnnotation\DoctrineAnnotationIndentationFixer
+        return 100;
+    }
+
+    public function getDefinition(): FixerDefinitionInterface
+    {
+        return new FixerDefinition('Nested annotation should start on standalone line', []);
+    }
+
+    /**
+     * @note indent is covered by
+     * @see \PhpCsFixer\Fixer\DoctrineAnnotation\DoctrineAnnotationIndentationFixer
+     *
+     * @param iterable<\PhpCsFixer\Doctrine\Annotation\Token>&Tokens $tokens
+     */
+    protected function fixAnnotations(Tokens $tokens): void
+    {
+        $this->currentBlockInfo = null;
+
+        $tokenCount = $tokens->count();
+
+        // what about foreach?
+        for ($index = 0; $index < $tokenCount; ++$index) {
+            /** @var Token $currentToken */
+            $currentToken = $tokens[$index];
+            if (! $currentToken->isType(DocLexer::T_AT)) {
+                continue;
+            }
+
+            /** @var Token|null $previousToken */
+            $previousTokenPosition = $index - 1;
+            $previousToken = $tokens[$previousTokenPosition] ?? null;
+            if ($previousToken === null) {
+                continue;
+            }
+
+            // docblock opener → skip it
+            if ($this->isDocOpener($previousToken)) {
+                continue;
+            }
+
+            $tokens->insertAt($index, new Token(DocLexer::T_NONE, ' * '));
+            $tokens->insertAt($index, new Token(DocLexer::T_NONE, "\n"));
+
+            // remove redundant space
+            if ($previousToken->isType(DocLexer::T_NONE)) {
+                $tokens->offsetUnset($previousTokenPosition);
+            }
+
+            $this->processEndBracket($index, $tokens, $previousTokenPosition);
+        }
+    }
+
+    private function isDocOpener(Token $token): bool
+    {
+        if ($token->isType(DocLexer::T_NONE)) {
+            return Strings::contains($token->getContent(), '*');
+        }
+
+        return false;
+    }
+
+    private function processEndBracket(int $index, Tokens $tokens, int $previousTokenPosition): void
+    {
+        /** @var Token $previousToken */
+        $previousToken = $tokens->offsetGet($previousTokenPosition);
+        // already a space → skip
+        if ($previousToken->isType(DocLexer::T_NONE)) {
+            return;
+        }
+
+        // reset
+        if ($this->currentBlockInfo !== null && ! $this->currentBlockInfo->contains($index)) {
+            $this->currentBlockInfo = null;
+            return;
+        }
+
+        if ($this->currentBlockInfo === null) {
+            $this->currentBlockInfo = $this->doctrineBlockFinder->findInTokensByEdge(
+                $tokens,
+                $previousTokenPosition
+            );
+        }
+
+        if ($this->currentBlockInfo !== null) {
+            $tokens->insertAt($this->currentBlockInfo->getEnd(), new Token(DocLexer::T_NONE, ' * '));
+            $tokens->insertAt($this->currentBlockInfo->getEnd(), new Token(DocLexer::T_NONE, "\n"));
+        }
+    }
+}
