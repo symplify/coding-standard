71dfc3771197b72b2274c934b84ab75ce6f9b3a2
TomasVotruba <tomas.vot@gmail.com>
2020-09-16 09:48:34 +0200

    open 8.4-dev

---
+++ tests/Fixer/LineLength/LineLengthFixer/ArrayLineLengthFixerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Fixer\LineLength\LineLengthFixer;
+
+use Iterator;
+use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+use Symplify\EasyCodingStandardTester\Testing\AbstractCheckerTestCase;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ArrayLineLengthFixerTest extends AbstractCheckerTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function provideData(): Iterator
+    {
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getCheckerClass(): string
+    {
+        return LineLengthFixer::class;
+    }
+}
