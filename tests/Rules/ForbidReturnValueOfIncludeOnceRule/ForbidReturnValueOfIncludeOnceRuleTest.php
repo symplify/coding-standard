a44bfb993cfb96224c6c2c59b8f721d75786958e
TomasVotruba <tomas.vot@gmail.com>
2020-09-20 10:59:06 +0200

    prepare release

---
+++ tests/Rules/ForbidReturnValueOfIncludeOnceRule/ForbidReturnValueOfIncludeOnceRuleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Rules\ForbidReturnValueOfIncludeOnceRule;
+
+use Iterator;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use Symplify\CodingStandard\Rules\ForbidReturnValueOfIncludeOnceRule;
+
+final class ForbidReturnValueOfIncludeOnceRuleTest extends RuleTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function testRule(string $filePath, array $expectedErrorMessagesWithLines): void
+    {
+        $this->analyse([$filePath], $expectedErrorMessagesWithLines);
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/ReturnRequireOnce.php', [[ForbidReturnValueOfIncludeOnceRule::ERROR_MESSAGE, 11]]];
+        yield [__DIR__ . '/Fixture/AssignRequireOnce.php', [[ForbidReturnValueOfIncludeOnceRule::ERROR_MESSAGE, 11]]];
+    }
+
+    protected function getRule(): Rule
+    {
+        return new ForbidReturnValueOfIncludeOnceRule();
+    }
+}
