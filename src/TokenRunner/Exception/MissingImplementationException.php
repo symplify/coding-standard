35be0279224272e93755ae7378855c6d847d2c03
TomasVotruba <tomas.vot@gmail.com>
2020-10-07 15:12:47 +0200

    prepare release

---
+++ src/TokenRunner/Exception/MissingImplementationException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\TokenRunner\Exception;
+
+use Exception;
+
+final class MissingImplementationException extends Exception
+{
+}
