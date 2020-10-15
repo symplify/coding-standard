e18d357f22298602a524b1a73c38b0ec1e13cc56
TomasVotruba <tomas.vot@gmail.com>
2020-10-15 15:38:51 +0200

    open 8.4-dev

---
+++ src/Rules/PrefferedMethodCallOverFuncCallRule.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use Symplify\CodingStandard\PhpParser\NodeNameResolver;
+
+/**
+ * @see \Symplify\CodingStandard\Tests\Rules\PrefferedMethodCallOverFuncCallRule\PrefferedMethodCallOverFuncCallRuleTest
+ */
+final class PrefferedMethodCallOverFuncCallRule extends AbstractPrefferedCallOverFuncRule
+{
+    /**
+     * @var string
+     */
+    public const ERROR_MESSAGE = 'Use "%s->%s()" method call over "%s()" func call';
+
+    /**
+     * @param array<string, string[]> $funcCallToPrefferedMethodCalls
+     */
+    public function __construct(NodeNameResolver $nodeNameResolver, array $funcCallToPrefferedMethodCalls = [])
+    {
+        parent::__construct($nodeNameResolver);
+
+        $this->funcCallToPrefferedCalls = $funcCallToPrefferedMethodCalls;
+    }
+
+    /**
+     * @param FuncCall $node
+     * @return string[]
+     */
+    public function process(Node $node, Scope $scope): array
+    {
+        $errorMessageParameters = $this->getErrorMessageParameters($node, $scope);
+        if ($errorMessageParameters === []) {
+            return [];
+        }
+
+        $errorMessage = sprintf(self::ERROR_MESSAGE, ...$errorMessageParameters);
+        return [$errorMessage];
+    }
+}
