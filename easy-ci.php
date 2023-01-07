dfe8c75172ab48ace44dbb5f34566bee6e0802c3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-07 23:19:43 +0700

    add tpyes to skip

--- easy-ci.php
+++ easy-ci.php
@@ -6,12 +6,8 @@ use Symplify\EasyCI\Config\EasyCIConfig;
 
 return static function (EasyCIConfig $easyCIConfig): void {
     $easyCIConfig->typesToSkip([
-        \Symplify\EasyCI\Config\EasyCIConfig::class,
-        \Symplify\EasyCI\Contract\Application\FileProcessorInterface::class,
-        \Symplify\EasyCI\Twig\TwigTemplateAnalyzer\ConstantPathTwigTemplateAnalyzer::class,
-        \Symplify\EasyCI\Twig\TwigTemplateAnalyzer\MissingClassConstantTwigAnalyzer::class,
-        \Symplify\EasyCI\ValueObject\ConfigFileSuffixes::class,
-        \Symplify\EasyCI\Console\EasyCIApplication::class,
-        \Symplify\EasyCI\Kernel\EasyCIKernel::class,
+        \Symplify\CodingStandard\TokenRunner\DocBlock\MalformWorker\InlineVariableDocBlockMalformWorker::class,
+        \Symplify\CodingStandard\TokenRunner\DocBlock\MalformWorker\MissingVarNameMalformWorker::class,
+        \Symplify\CodingStandard\TokenRunner\DocBlock\MalformWorker\ParamNameReferenceMalformWorker::class,
     ]);
 };
