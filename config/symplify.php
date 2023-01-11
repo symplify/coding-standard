a4c1d8416ecae940a9ac4a4a0faae3c8ec6ed7c3
Tomas Votruba <tomas.vot@gmail.com>
2023-01-11 12:12:31 +0100

    make set explicit

--- config/symplify.php
+++ config/symplify.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ClassNotation\FinalInternalClassFixer;
 use PhpCsFixer\Fixer\Phpdoc\GeneralPhpdocAnnotationRemoveFixer;
 use Symplify\CodingStandard\Fixer\Spacing\MethodChainingNewlineFixer;
 use Symplify\CodingStandard\Fixer\Spacing\NewlineServiceDefinitionConfigFixer;
@@ -15,13 +14,30 @@ return static function (ECSConfig $ecsConfig): void {
 
     // epxpclit like other configs :) no magic!!!
 
-
     $ecsConfig->rules([
+        // docblocks and comments
+        \Symplify\CodingStandard\Fixer\Annotation\RemovePHPStormAnnotationFixer::class,
+        \Symplify\CodingStandard\Fixer\Commenting\ParamReturnAndVarTagMalformsFixer::class,
+        \Symplify\CodingStandard\Fixer\Commenting\RemoveUselessDefaultCommentFixer::class,
+
+        // arrays
+        \Symplify\CodingStandard\Fixer\ArrayNotation\ArrayListItemNewlineFixer::class,
+        \Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer::class,
+        StandaloneLinePromotedPropertyFixer::class,
+
         // newlines
         MethodChainingNewlineFixer::class,
         NewlineServiceDefinitionConfigFixer::class,
         SpaceAfterCommaHereNowDocFixer::class,
         StandaloneLinePromotedPropertyFixer::class,
+        \Symplify\CodingStandard\Fixer\Strict\BlankLineAfterStrictTypesFixer::class,
+
+        // line length
+        \Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer::class,
+
+        // naming
+        \Symplify\CodingStandard\Fixer\Naming\StandardizeHereNowDocKeywordFixer::class,
+
     ]);
 
     $ecsConfig->ruleWithConfiguration(GeneralPhpdocAnnotationRemoveFixer::class, [
