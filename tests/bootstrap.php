199f1e5e2b2bd67eac392ab1d3c6c04bf7ebce3c
Tomas Votruba <tomas.vot@gmail.com>
2023-12-07 10:18:34 +0100

    fix path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use PHP_CodeSniffer\Util\Tokens;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/phpcsstandards/php_codesniffer/autoload.php';
+require_once __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
 
 // initalize token with INT type, otherwise php-cs-fixer and php-parser breaks
 // initalize token with INT type, otherwise php-cs-fixer and php-parser breaks
