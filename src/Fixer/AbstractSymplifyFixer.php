bbfc662f70a8746b874003ecd4f90d54edf1bf55
Michael Wedderburn <mdk999@gmail.com>
2024-09-27 11:33:07 -0700

    Set the fixer name statically so that cs-fixer and import

--- src/Fixer/AbstractSymplifyFixer.php
+++ src/Fixer/AbstractSymplifyFixer.php
@@ -20,7 +20,7 @@ abstract class AbstractSymplifyFixer implements FixerInterface
 
     public function getName(): string
     {
-        return static::class;
+        return 'Nocworx/codingstandard';
     }
 
     public function isRisky(): bool
