556e579b1ca3b3142df1390892b17631ae67cc21
Tomas Votruba <tomas.vot@gmail.com>
2025-04-28 00:10:39 +0200

    [annotation] Add RemoveSetterGetterDocblockFixer

---
+++ tests/Fixer/Annotation/RemoveSetterGetterDocblockFixer/RemoveSetterGetterDocblockFixerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\Tests\Fixer\Annotation\RemoveSetterGetterDocblockFixer;
+
+use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Symplify\EasyCodingStandard\Testing\PHPUnit\AbstractCheckerTestCase;
+
+final class RemoveSetterGetterDocblockFixerTest extends AbstractCheckerTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFiles(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfig(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
