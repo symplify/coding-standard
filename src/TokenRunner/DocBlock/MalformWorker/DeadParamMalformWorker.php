6a5d2aeb1f6c671c7022818a18ada2f952324d02
Tomas Votruba <tomas.vot@gmail.com>
2026-06-09 21:03:59 +0100

    extend dead param worker (#84)

---
+++ src/TokenRunner/DocBlock/MalformWorker/DeadParamMalformWorker.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\CodingStandard\TokenRunner\DocBlock\MalformWorker;
+
+use Nette\Utils\Strings;
+use PhpCsFixer\DocBlock\DocBlock;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+use Symplify\CodingStandard\TokenRunner\Contract\DocBlock\MalformWorkerInterface;
+
+/**
+ * Removes dead param annotation lines that only contain a variable name and no type,
+ * e.g. "param $value" - such line carries no information and can be removed.
+ */
+final class DeadParamMalformWorker implements MalformWorkerInterface
+{
+    /**
+     * @see https://regex101.com/r/Hk4lFc/1
+     */
+    private const string PARAM_WITHOUT_TYPE_REGEX = '#@(?:psalm-|phpstan-)?param\s+\$\w+\s*$#';
+
+    /**
+     * @param Tokens<Token> $tokens
+     */
+    public function work(string $docContent, Tokens $tokens, int $position): string
+    {
+        $docBlock = new DocBlock($docContent);
+
+        foreach ($docBlock->getLines() as $line) {
+            if (! Strings::match($line->getContent(), self::PARAM_WITHOUT_TYPE_REGEX)) {
+                continue;
+            }
+
+            $line->remove();
+        }
+
+        return $docBlock->getContent();
+    }
+}
